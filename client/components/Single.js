8f12541e369ddaad3c6636858cf789bed039aa51
Linsy Joyner <Linsy.Joyner@gmail.com>
2016-12-01 20:04:04 -0500

    created reducers, populated photos and likes

---
+++ client/components/Single.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Single extends Component {
+  render() {
+    return (
+      <div>
+      <h1>
+        Im the Single
+      </h1>
+      </div>
+    )
+  }
+}
+
+export default Single;
